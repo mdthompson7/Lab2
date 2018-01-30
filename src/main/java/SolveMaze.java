b338048de8c3305035278cd7e94945bcce6d461b
mdthompson7 <mdt3@illinois.edu>
2018-01-30 14:42:22 -0600

    cleaned code, tightened move limit

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -33,30 +33,27 @@ public class SolveMaze {
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
-        for (int step = 0; step < 10000; step++) {
+        for (int step = 0; step < 200; step++) {
             // Implement your maze solving algorithm here
             while (!maze.isFinished()) {
                 maze.turnLeft();
                 if (maze.canMove()) {
                     maze.move();
-                    count++;
                 } else {
                     maze.turnRight();
                     if (maze.canMove()) {
                         maze.move();
-                        count++;
                     } else {
                         maze.turnRight();
                         if (maze.canMove()) {
                             maze.move();
-                            count++;
                         } else {
                             maze.turnRight();
                             maze.move();
-                            count++;
                         }
                     }
                 }
+                count++;
             }
         }
 
